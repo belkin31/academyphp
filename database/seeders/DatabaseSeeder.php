088e9e35b7f97227c831db623aa49ca0fd4565a2
Artem <tema.belka@gmail.com>
2025-10-29 17:02:28 +0700

    add models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,23 +1,23 @@
 <?php
-
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            MerchantSeeder::class,
+            MerchantPersonalSeeder::class,
+            SaleStatusSeeder::class,
+            ProductGroupSeeder::class,
+            ProductSeeder::class,
+            ClientSeeder::class,
+            TaskSeeder::class,
+            SaleSeeder::class,
+            ProductOperationSeeder::class,
+            MerchantSettingSeeder::class,
         ]);
     }
 }
